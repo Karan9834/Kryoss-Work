e4ecf61e1c4c165de2e83db3347235b5a842bbf9
karan7638 <karan2421kumar@gmail.com>
2026-02-27 12:20:45 +0530

    feat: add Taxi Bidding App like inDrive page and route

--- src/routes/ProductRoutes.jsx
+++ src/routes/ProductRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import TaxiBookingUber from '../pages/products/transport-app-solutions/TaxiBookingUber';
+import TaxiBiddingApp from '../pages/products/transport-app-solutions/TaxiBiddingApp';
 import FoodDeliveryApp from '../pages/products/delivery-app-solutions/FoodDeliveryApp';
 
 export const ProductRoutes = [
     <Route key="taxi-uber" path="products/transport/uber-clone" element={<TaxiBookingUber />} />,
+    <Route key="taxi-bidding" path="products/transport/indrive-clone" element={<TaxiBiddingApp />} />,
     <Route key="food-delivery" path="products/delivery/food-delivery" element={<FoodDeliveryApp />} />,
     // Add other product routes here
 ];
