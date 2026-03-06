8c538cc5235326dddc2097d82411c64e110a3184
Adarsh Kumar Pathak <adarsh21122004@gmail.com>
2026-03-06 18:14:55 +0530

    New Page Added Cannabis and Vedio-App in Social Media Section

--- src/routes/ProductRoutes.jsx
+++ src/routes/ProductRoutes.jsx
@@ -1,102 +1,3 @@
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-
-// import TaxiBookingUber from '../pages/products/transport-app-solutions/TaxiBookingUber';
-// import TaxiBiddingApp from '../pages/products/transport-app-solutions/TaxiBiddingApp';
-// import BikeRidingApp from '../pages/products/transport-app-solutions/BikeRidingApp';
-// import CarPoolingApp from "../pages/products/transport-app-solutions/CarPoolingApp";
-// import CarRental from "../pages/products/transport-app-solutions/CarRental";
-
-// import FoodDeliveryApp from '../pages/products/delivery-app-solutions/FoodDeliveryApp';
-// import GroceryDeliveryApp from '../pages/products/delivery-app-solutions/GroceryDeliveryApp';
-// import MedicineDeliveryApp from '../pages/products/delivery-app-solutions/MedicineDeliveryApp';
-
-// import DailyDeliveryApp from '../pages/products/delivery-app-solutions/DailyDeliveryApp';
-// import LiquorDeliveryApp from '../pages/products/delivery-app-solutions/LiquorDeliveryApp';
-
-// import TutorBookingApp from '../pages/products/Ondemand-homeservice/TutorBookingApp';
-// import Handyman from "../pages/products/Ondemand-homeservice/Handyman_service";
-// import HomeCleaning from "../pages/products/Ondemand-homeservice/HomeCleaning";
-// import DogWalking from '../pages/products/Ondemand-homeservice/DogWalking';
-// import PestControl from '../pages/products/Ondemand-homeservice/PestControl';
-// import SalonBeauty from '../pages/products/Ondemand-homeservice/SalonBeauty';
-// import MassageTherapy from '../pages/products/Ondemand-homeservice/MassageTherapy';
-// import TowTruck from "../pages/products/Ondemand-homeservice/TowTruck";
-// import LaundryService from "../pages/products/Ondemand-homeservice/LaundryService";
-// import DatingApp from '../pages/products/Emerging-Social App/DatingApp';
-// import AppointmentBooking from "../pages/products/Emerging-Social App/AppointmentBooking";
-
-
-// export const ProductRoutes = [
-//     <Route key="taxi-uber" path="products/transport/uber-clone" element={<TaxiBookingUber />} />,
-//     <Route key="taxi-bidding" path="products/transport/indrive-clone" element={<TaxiBiddingApp />} />,
-//     <Route key="bike-riding" path="products/transport/rapido-clone" element={<BikeRidingApp />} />,
-//     <Route key="car-pooling" path="products/transport/blablacar-clone" element={<CarPoolingApp />} />,
-//     <Route key="car-rental" path="products/transport/car-rental" element={<CarRental />} />,
-//     <Route key="food-delivery" path="products/delivery/food-delivery" element={<FoodDeliveryApp />} />,
-//     <Route key="grocery-delivery" path="products/delivery/grocery-delivery" element={<GroceryDeliveryApp />} />,
-//     // Add other product routes here
-//    <Route 
-//   key="home-cleaning"
-//   path="products/transport/blablacar-clone"
-//   element={<CarPoolingApp />}
-// />,
-
-//  <Route key="home-cleaning" path="products/home-service/cleaning" element={<HomeCleaning />} />,
-
-//   <Route key="home-cleaning" path="products/home-service/salon" element={<SalonBeauty />} />,
-//    <Route key="home-cleaning" path="products/home-service/massage" element={<MassageTherapy />} />,
-//  <Route key="home-cleaning" path="products/home-service/cleaning" element={<HomeCleaning />} />,
-//     <Route key="home-cleaning" path="products/transport/blablacar-clone" element={<CarPoolingApp />} />,
-
-//         <Route key="daily-delivery" path="products/delivery/daily-delivery" element={<DailyDeliveryApp />} />,
-//             <Route key="liquor-delivery" path="products/delivery/liquor-delivery" element={<LiquorDeliveryApp />} />,
-
-
-//     <Route key="medicine-delivery" path="products/delivery/medicine-delivery" element={<MedicineDeliveryApp />} />,
-//     <Route key="tutor-booking" path="products/home-service/tutor" element={<TutorBookingApp />} />,
-//     <Route key="home-cleaning" path="products/home-service/cleaning" element={<HomeCleaning />} />,
-//     <Route key="handyman" path="products/home-service/handyman" element={<Handyman />} />,
-//     <Route key="dog-walking" path="products/home-service/dog-walking" element={<DogWalking />} />,
-//     <Route key="pest-control" path="products/home-service/pest-control" element={<PestControl />} />,
-
-//     <Route key="salon-beauty" path="products/home-service/salon" element={<SalonBeauty />} />,
-//     <Route key="massage-therapy" path="products/home-service/massage" element={<MassageTherapy />} />,
-//     <Route key="handyman" path="products/home-service/handyman" element={<Handyman />} />,
-//     <Route key="tow-truck" path="products/home-service/tow-truck" element={<TowTruck />} />,
-//     <Route key="laundry-service" path="products/home-service/laundry" element={<LaundryService />} />,
-//     <Route key="dating-app" path="products/social/dating-app" element={<DatingApp />} />,
-//     <Route key="appointment-booking" path="products/social/appointment-booking" element={<AppointmentBooking />} />,
-// ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { Route } from "react-router-dom";
 
@@ -106,18 +7,12 @@ import BikeRidingApp from "../pages/products/transport-app-solutions/BikeRidingA
 import CarPoolingApp from "../pages/products/transport-app-solutions/CarPoolingApp";
 import CarRental from "../pages/products/transport-app-solutions/CarRental";
 
-<<<<<<< Updated upstream
 import FoodDeliveryApp from "../pages/products/delivery-app-solutions/FoodDeliveryApp";
 import GroceryDeliveryApp from "../pages/products/delivery-app-solutions/GroceryDeliveryApp";
 import MedicineDeliveryApp from "../pages/products/delivery-app-solutions/MedicineDeliveryApp";
 import DailyDeliveryApp from "../pages/products/delivery-app-solutions/DailyDeliveryApp";
 import LiquorDeliveryApp from "../pages/products/delivery-app-solutions/LiquorDeliveryApp";
-=======
-import FoodDeliveryApp from '../pages/products/delivery-app-solutions/FoodDeliveryApp';
-import GroceryDeliveryApp from '../pages/products/delivery-app-solutions/GroceryDeliveryApp';
-import MedicineDeliveryApp from '../pages/products/delivery-app-solutions/MedicineDeliveryApp';
-import CannabisDeliveryApp from '../pages/products/delivery-app-solutions/Cannabis-Delivery-App';
->>>>>>> Stashed changes
+import CannabisDeliveryApp from "../pages/products/delivery-app-solutions/Cannabis-Delivery-App";
 
 import TutorBookingApp from "../pages/products/Ondemand-homeservice/TutorBookingApp";
 import Handyman from "../pages/products/Ondemand-homeservice/Handyman_service";
@@ -128,67 +23,36 @@ import SalonBeauty from "../pages/products/Ondemand-homeservice/SalonBeauty";
 import MassageTherapy from "../pages/products/Ondemand-homeservice/MassageTherapy";
 import TowTruck from "../pages/products/Ondemand-homeservice/TowTruck";
 import LaundryService from "../pages/products/Ondemand-homeservice/LaundryService";
-import TikTokClonePage from "../pages/products/Social-App-Solutions/Video-App";
 
-<<<<<<< Updated upstream
 import DatingApp from "../pages/products/Emerging-Social App/DatingApp";
 import AppointmentBooking from "../pages/products/Emerging-Social App/AppointmentBooking";
+import TikTokClonePage from "../pages/products/Social-App-Solutions/Video-App";
 
-export const ProductRoutes = [
-  <Route key="taxi-uber" path="products/transport/uber-clone" element={<TaxiBookingUber />} />,
-  <Route key="taxi-bidding" path="products/transport/indrive-clone" element={<TaxiBiddingApp />} />,
-  <Route key="bike-riding" path="products/transport/rapido-clone" element={<BikeRidingApp />} />,
-  <Route key="car-pooling" path="products/transport/blablacar-clone" element={<CarPoolingApp />} />,
-  <Route key="car-rental" path="products/transport/car-rental" element={<CarRental />} />,
-
-  <Route key="food-delivery" path="products/delivery/food-delivery" element={<FoodDeliveryApp />} />,
-  <Route key="grocery-delivery" path="products/delivery/grocery-delivery" element={<GroceryDeliveryApp />} />,
-  <Route key="medicine-delivery" path="products/delivery/medicine-delivery" element={<MedicineDeliveryApp />} />,
-  <Route key="daily-delivery" path="products/delivery/daily-delivery" element={<DailyDeliveryApp />} />,
-  <Route key="liquor-delivery" path="products/delivery/liquor-delivery" element={<LiquorDeliveryApp />} />,
-
-  <Route key="tutor-booking" path="products/home-service/tutor" element={<TutorBookingApp />} />,
-  <Route key="home-cleaning" path="products/home-service/cleaning" element={<HomeCleaning />} />,
-  <Route key="handyman" path="products/home-service/handyman" element={<Handyman />} />,
-  <Route key="dog-walking" path="products/home-service/dog-walking" element={<DogWalking />} />,
-  <Route key="pest-control" path="products/home-service/pest-control" element={<PestControl />} />,
-  <Route key="salon-beauty" path="products/home-service/salon" element={<SalonBeauty />} />,
-  <Route key="massage-therapy" path="products/home-service/massage" element={<MassageTherapy />} />,
-  <Route key="tow-truck" path="products/home-service/tow-truck" element={<TowTruck />} />,
-  <Route key="laundry-service" path="products/home-service/laundry" element={<LaundryService />} />,
-
-  <Route key="dating-app" path="products/social/dating-app" element={<DatingApp />} />,
-  <Route key="appointment-booking" path="products/social/appointment-booking" element={<AppointmentBooking />} />,
-=======
 export const ProductRoutes = [
     <Route key="taxi-uber" path="products/transport/uber-clone" element={<TaxiBookingUber />} />,
     <Route key="taxi-bidding" path="products/transport/indrive-clone" element={<TaxiBiddingApp />} />,
     <Route key="bike-riding" path="products/transport/rapido-clone" element={<BikeRidingApp />} />,
     <Route key="car-pooling" path="products/transport/blablacar-clone" element={<CarPoolingApp />} />,
     <Route key="car-rental" path="products/transport/car-rental" element={<CarRental />} />,
+
     <Route key="food-delivery" path="products/delivery/food-delivery" element={<FoodDeliveryApp />} />,
     <Route key="grocery-delivery" path="products/delivery/grocery-delivery" element={<GroceryDeliveryApp />} />,
     <Route key="medicine-delivery" path="products/delivery/medicine-delivery" element={<MedicineDeliveryApp />} />,
+    <Route key="daily-delivery" path="products/delivery/daily-delivery" element={<DailyDeliveryApp />} />,
+    <Route key="liquor-delivery" path="products/delivery/liquor-delivery" element={<LiquorDeliveryApp />} />,
     <Route key="cannabis-delivery" path="products/delivery/cannabis-delivery" element={<CannabisDeliveryApp />} />,
+
     <Route key="tutor-booking" path="products/home-service/tutor" element={<TutorBookingApp />} />,
     <Route key="home-cleaning" path="products/home-service/cleaning" element={<HomeCleaning />} />,
     <Route key="handyman" path="products/home-service/handyman" element={<Handyman />} />,
     <Route key="dog-walking" path="products/home-service/dog-walking" element={<DogWalking />} />,
     <Route key="pest-control" path="products/home-service/pest-control" element={<PestControl />} />,
-
     <Route key="salon-beauty" path="products/home-service/salon" element={<SalonBeauty />} />,
     <Route key="massage-therapy" path="products/home-service/massage" element={<MassageTherapy />} />,
-    <Route key="handyman" path="products/home-service/handyman" element={<Handyman />} />,
     <Route key="tow-truck" path="products/home-service/tow-truck" element={<TowTruck />} />,
     <Route key="laundry-service" path="products/home-service/laundry" element={<LaundryService />} />,
+
+    <Route key="dating-app" path="products/social/dating-app" element={<DatingApp />} />,
+    <Route key="appointment-booking" path="products/social/appointment-booking" element={<AppointmentBooking />} />,
     <Route key="video-app" path="products/social/video-app" element={<TikTokClonePage />} />
->>>>>>> Stashed changes
 ];
-
-
-
-
-
-
-
-
