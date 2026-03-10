d57decf22ca76ebff02a222eac23adc9b2f7a614
Riya Agrawal <RiyaAgrawal2005@gmail.com>
2026-03-10 15:56:19 +0530

    Added product page, testimonials, life at kryoss section and contact updates

---
+++ src/pages/company/Testimonial.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import TestimonialSection from "../../components/Client-Testimonials/TestimonialSection";
+import GoogleReviews from '../../components/Client-Testimonials/GoogleReviews';
+
+
+
+const TestimonialPage = () => {
+    return(
+        <>
+        <TestimonialSection/>
+        <GoogleReviews/>
+        </>
+    );
+};
+
+export default TestimonialPage;
--- src/routes/CompanyRoutes.jsx
+++ src/routes/CompanyRoutes.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import AboutPage from '../pages/company/About';
 import ContactPage from '../pages/company/Contact';
+import TestimonialPage from '../pages/company/Testimonial';
 import HealthcareSolution from '../pages/solutions/Healthcare';
+import LifeAtKryossWorkPage from '../pages/company/LifeArKryossWork';
 
 export const CompanyRoutes = [
     <Route key="company-about" path="company/about" element={<AboutPage />} />,
     <Route key="company-contact" path="company/contact" element={<ContactPage />} />,
+    <Route key="company-testimonial" path="company/testimonials" element={<TestimonialPage />} />,
+        <Route key="company-life" path="company/life-at-kryosswork" element={<LifeAtKryossWorkPage />} />,
     <Route key="solutions-healthcare" path="solutions/healthcare" element={<HealthcareSolution />} />,
     // Add other company/solution routes here
 ];
