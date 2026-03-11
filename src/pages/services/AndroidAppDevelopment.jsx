926d2a3bbd907159eca35a7a1aa103372c23171f
karan7638 <karan2421kumar@gmail.com>
2026-03-11 18:35:14 +0530

    fix: lowercase services-app folder import casing for linux

--- src/pages/services/AndroidAppDevelopment.jsx
+++ src/pages/services/AndroidAppDevelopment.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import AndroidHero from '@/components/Services-app/android-app/AndroidHero';
-import AndroidWhatWeOffer from '@/components/Services-app/android-app/AndroidWhatWeOffer';
-import AndroidProcess from '@/components/Services-app/android-app/AndroidProcess';
-import AndroidWhyItWork from '@/components/Services-app/android-app/AndroidWhyItWork';
-import AndroidTechnologyStack from '@/components/Services-app/android-app/AndroidTechnologyStack';
-import AndroidCTA1 from '@/components/Services-app/android-app/AndroidCTA1';
-import AndroidFAQ from '@/components/Services-app/android-app/AndroidFAQ';
-import AndroidWhyUs from '@/components/Services-app/android-app/AndroidWhyUs';
-import AndroidIndustries from '@/components/Services-app/android-app/AndroidIndustries';
-import AndroidCTA2 from '@/components/Services-app/android-app/AndroidCTA2';
-import AndroidContact from '@/components/Services-app/android-app/AndroidContact';
-import AndroidBlogs from '@/components/Services-app/android-app/AndroidBlogs';
-import AndroidGetUpdates from '@/components/Services-app/android-app/AndroidGetUpdates';
+import AndroidHero from '@/components/services-app/android-app/AndroidHero';
+import AndroidWhatWeOffer from '@/components/services-app/android-app/AndroidWhatWeOffer';
+import AndroidProcess from '@/components/services-app/android-app/AndroidProcess';
+import AndroidWhyItWork from '@/components/services-app/android-app/AndroidWhyItWork';
+import AndroidTechnologyStack from '@/components/services-app/android-app/AndroidTechnologyStack';
+import AndroidCTA1 from '@/components/services-app/android-app/AndroidCTA1';
+import AndroidFAQ from '@/components/services-app/android-app/AndroidFAQ';
+import AndroidWhyUs from '@/components/services-app/android-app/AndroidWhyUs';
+import AndroidIndustries from '@/components/services-app/android-app/AndroidIndustries';
+import AndroidCTA2 from '@/components/services-app/android-app/AndroidCTA2';
+import AndroidContact from '@/components/services-app/android-app/AndroidContact';
+import AndroidBlogs from '@/components/services-app/android-app/AndroidBlogs';
+import AndroidGetUpdates from '@/components/services-app/android-app/AndroidGetUpdates';
 
 
 
--- src/pages/services/KotlinAppDevelopment.jsx
+++ src/pages/services/KotlinAppDevelopment.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import KotlinHero from '@/components/Services-app/kotlin-app/KotlinHero';
-import KotlinSolutions from '@/components/Services-app/kotlin-app/KotlinSolutions';
-import KotlinWorkflow from '@/components/Services-app/kotlin-app/KotlinWorkflow';
-import KotlinStrategicAdvantages from '@/components/Services-app/kotlin-app/KotlinStrategicAdvantages';
-import KotlinModernTechnology from '@/components/Services-app/kotlin-app/KotlinModernTechnology';
-import KotlinCTA1 from '@/components/Services-app/kotlin-app/KotlinCTA1';
-import KotlinFAQ from '@/components/Services-app/kotlin-app/KotlinFAQ';
-import KotlinWhatMakeUsBest from '@/components/Services-app/kotlin-app/KotlinWhatMakeUsBest';
-import KotlinIndustries from '@/components/Services-app/kotlin-app/KotlinIndustries';
-import KotlinCTA2 from '@/components/Services-app/kotlin-app/KotlinCTA2';
-import KotlinContact from '@/components/Services-app/kotlin-app/KotlinContact';
-import KotlinBlogs from '@/components/Services-app/kotlin-app/KotlinBlogs';
-import KotlinGetUpdates from '@/components/Services-app/kotlin-app/KotlinGetUpdates';
+import KotlinHero from '@/components/services-app/kotlin-app/KotlinHero';
+import KotlinSolutions from '@/components/services-app/kotlin-app/KotlinSolutions';
+import KotlinWorkflow from '@/components/services-app/kotlin-app/KotlinWorkflow';
+import KotlinStrategicAdvantages from '@/components/services-app/kotlin-app/KotlinStrategicAdvantages';
+import KotlinModernTechnology from '@/components/services-app/kotlin-app/KotlinModernTechnology';
+import KotlinCTA1 from '@/components/services-app/kotlin-app/KotlinCTA1';
+import KotlinFAQ from '@/components/services-app/kotlin-app/KotlinFAQ';
+import KotlinWhatMakeUsBest from '@/components/services-app/kotlin-app/KotlinWhatMakeUsBest';
+import KotlinIndustries from '@/components/services-app/kotlin-app/KotlinIndustries';
+import KotlinCTA2 from '@/components/services-app/kotlin-app/KotlinCTA2';
+import KotlinContact from '@/components/services-app/kotlin-app/KotlinContact';
+import KotlinBlogs from '@/components/services-app/kotlin-app/KotlinBlogs';
+import KotlinGetUpdates from '@/components/services-app/kotlin-app/KotlinGetUpdates';
 const KotlinAppDevelopment = () => {
     return (
         <>
